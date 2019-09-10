12b086b7489b25c6e712d3e4c020d75ae551451e
Bayo Oyekole <oyebayo@gmail.com>
2019-09-10 15:42:22 +0400

    Create skel-init.cmd
    Used to initiate a new local repository to meet team standards

---
+++ skel-init.cmd
@@ -0,0 +1,12 @@
+@echo off
+set dirlist=build\web build\app build\console build\service data\schema data\sample code docs lib res
+set commit_message=Initialize repository with folder structure
+mkdir %1
+cd %1
+git init
+mkdir %dirlist%
+for %%i in (. %dirlist%) do echo.>%%i\.gitignore
+echo.>README.md
+git add -A
+git commit -m "%commit_message%"
+cd ..
